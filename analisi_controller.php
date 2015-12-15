2bee6779d2900a181c0215105c81c5153046ee96
joker92 <euroseuff@gmail.com>
2015-12-15 12:43:15 +0100

    file modified

---
+++ analisi_controller.php
@@ -0,0 +1,42 @@
+
+
+<?php
+	$testo="";
+	if(isset($_POST["testo"])){
+
+		$testo=$_POST["testo"];
+
+		}
+	
+ //explode serve per contare le parole che ci sono nel testo inserito 
+ $testo_arr = explode( " " , $testo  );
+
+	//echo "numero parole=". count($testo_arr);
+
+        //metto il commento sopra all echo per continuare con un foreach per contare le parole piu frequenti
+
+
+		$frequenzaparole=[] ;
+
+		foreach($testo_arr as $parola) {
+			if(isset($frequenzaparole[$parola])){
+				$frequenzaparole[$parola]++;
+			} else {
+				$frequenzaparole[$parola]=1;
+
+			}
+
+		}
+
+		arsort($frequenzaparole);
+
+		foreach($frequenzaparole as $parola=>$conteggio){
+
+			echo $parola . "=". $conteggio . "<br />"; 
+
+
+		}
+
+
+?>
+
---
+++ analisitesto.php
@@ -0,0 +1,18 @@
+
+
+analisitesto.php
+
+<html>
+
+		<!--..dentro il comando action va arichiamare la pagina che abbiamo inserito visualizzando la variabile ..-->
+	<form method="post" action="analisi_controller.php">
+
+		<textarea name="testo" cols="60" rows="10"></textarea>
+
+		<input type="submit"/>
+
+
+	</from>
+</html>
+
+
