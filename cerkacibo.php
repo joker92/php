6d825ee00cd5e2ed349d1dad1e67868dcca61aa3
joker92 <euroseuff@gmail.com>
2016-01-12 12:56:09 +0100

    new file php

--- cerkacibo.php
+++ cerkacibo.php
@@ -14,8 +14,10 @@
 		//qui sonon sicuro che "chiave"
 
 		//e definita
-		foreach ($cibieingredienti as $cibo => $ingr){
 
+
+		foreach ($cibieingredienti as $cibo => $ingr){
+//il codice qui sotto serve per trovare le parole che cerchiamo esempio (qui gli ingredienti  )
 			if(strpos($ingr, $_GET["chiave"]) > -1) {
 				echo  $cibo ;
 				echo " (" . $ingr . ")<br/>";
